9f53cabeb0ae50b4ef76f2823637429727e87ed9
marble <git@computer-in.love>
2023-04-17 23:31:17 +0200

    added utime

--- ports/rtl8762c/mpconfigport.h
+++ ports/rtl8762c/mpconfigport.h
@@ -18,6 +18,8 @@
 #define MICROPY_PY_MATH                         (0)
 #define MICROPY_PY_IO                           (0)
 #define MICROPY_PY_STRUCT                       (0)
+#define MICROPY_PY_UTIME_MP_HAL                 (1)
+
 
 // extended modules
 #define MICROPY_PY_MACHINE          (1)
--- ports/rtl8762c/mphalport.h
+++ ports/rtl8762c/mphalport.h
@@ -1,6 +1,29 @@
 #ifndef INCLUDED_MPHALPORT_H
 #define INCLUDED_MPHALPORT_H
 
+#include "platform/platform_utils.h"
+#include "extmod/utime_mphal.h"
+
 static inline void mp_hal_set_interrupt_char(char c) {}
+
+static inline mp_uint_t mp_hal_ticks_cpu(void) {
+    return platform_vendor_tick();
+}
+
+static inline mp_uint_t mp_hal_ticks_us(void) {
+    return platform_vendor_tick()*(CLOCK_40MHZ/1000000u);
+}
+
+static inline mp_uint_t mp_hal_ticks_ms(void) {
+    return platform_vendor_tick()*(CLOCK_40MHZ/1000);
+}
+
+static inline void mp_hal_delay_ms(mp_uint_t time_ms) {
+    platform_delay_ms(time_ms);
+}
+
+static inline void mp_hal_delay_us(mp_uint_t time_us) {
+    platform_delay_us(time_us);
+}
 
 #endif // INCLUDED_MPHALPORT_H
